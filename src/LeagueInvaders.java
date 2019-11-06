8852dfce75578efa38742a8a3d728c65ebda628e
League Student <league@iMac-10.attlocal.net>
2019-11-05 18:26:09 -0800

    League Invaders IPPPPPPPPPPPPPPPPPPPPPPPPPPP

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,16 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+JFrame frame;
+static final int WIDTH = 500;
+static final int HEIGHT = 800;
+	public static void main(String[] args) {
+	
+}
+	public LeagueInvaders(){
+		frame = new JFrame();
+	}
+	setup(){
+		
+	}
 }
