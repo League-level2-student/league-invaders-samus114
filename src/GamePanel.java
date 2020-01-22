42a04dbb7e660fadbe72fe15f31678e2b5c9cbad
League Student <league@iMac-15.attlocal.net>
2020-01-21 18:30:06 -0800

    ip league invaders

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,112 +5,133 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Rocketship rocket = new Rocketship(250, 700, 50, 50);
 	final int MENU = 0;
-    final int GAME = 1;
-    final int END = 2;
-    Timer frameDraw;
-    int currentState = MENU;
-    ObjectManager object;
-    Font titleFont = new Font("Arial", Font.PLAIN, 48);
-    Font subFont = new Font("Arial", Font.PLAIN, 26);
-    GamePanel() {
-    	frameDraw = new Timer(1000/60,this);
-	    frameDraw.start();
-	    object = new ObjectManager(rocket);
-    }
-    @Override
-	public void paintComponent(Graphics g) {
-    	if(currentState == MENU){
-    	    drawMenuState(g);
-    	}else if(currentState == GAME){
-    	    drawGameState(g);
-    	}else if(currentState == END){
-    	    drawEndState(g);
-    	}
+	final int GAME = 1;
+	final int END = 2;
+	Timer frameDraw;
+	int currentState = MENU;
+	ObjectManager object;
+	Font titleFont = new Font("Arial", Font.PLAIN, 48);
+	Font subFont = new Font("Arial", Font.PLAIN, 26);
+
+	GamePanel() {
+		frameDraw = new Timer(1000 / 60, this);
+		frameDraw.start();
+		object = new ObjectManager(rocket);
 	}
-    void updateMenuState() {
-    	
-    }
-    void updateGameState() {
-    	object.update();
-    	
-    }
-    void updateEndState()  {
-    	
-    }
-    void drawMenuState(Graphics g) {
-    	g.setColor(Color.BLUE);
-    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-    	g.setFont(titleFont);
-    	g.setColor(Color.YELLOW);
-    	g.drawString("LEAGUE INVADERS", 25, 100);
-    	g.setFont(subFont);
-    	g.drawString("Press ENTER to start", 100, 300);
-    	g.drawString("Press SPACE for instructions", 50, 400);
-    }
-    void drawGameState(Graphics g) {
-    	g.setColor(Color.BLACK);
-    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-object.draw(g);
-    }
-    void drawEndState(Graphics g)  {
-    	g.setColor(Color.RED);
-    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-    	g.setColor(Color.YELLOW);
-    	g.setFont(titleFont);
-    	g.drawString("GAME OVER", 100, 100);
-    	g.setFont(subFont);
-    	g.drawString("You killed enemies", 100, 200);
-    	g.drawString("Press ENTER to restart", 80, 400);
-    }
+
+	@Override
+	public void paintComponent(Graphics g) {
+		if (currentState == MENU) {
+			drawMenuState(g);
+		} else if (currentState == GAME) {
+			drawGameState(g);
+		} else if (currentState == END) {
+			drawEndState(g);
+		}
+	}
+
+	void updateMenuState() {
+
+	}
+
+	void updateGameState() {
+		object.update();
+
+	}
+
+	void updateEndState() {
+
+	}
+
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("LEAGUE INVADERS", 25, 100);
+		g.setFont(subFont);
+		g.drawString("Press ENTER to start", 100, 300);
+		g.drawString("Press SPACE for instructions", 50, 400);
+	}
+
+	void drawGameState(Graphics g) {
+        BufferedImage image = null;
+		try {
+			image = ImageIO.read(this.getClass().getResourceAsStream("space.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		g.drawImage(image, 0, 0, WIDTH, HEIGHT, null);
+		object.draw(g);
+	}
+
+	void drawEndState(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setColor(Color.YELLOW);
+		g.setFont(titleFont);
+		g.drawString("GAME OVER", 100, 100);
+		g.setFont(subFont);
+		g.drawString("You killed enemies", 100, 200);
+		g.drawString("Press ENTER to restart", 80, 400);
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("action");
-		if(currentState == MENU){
-		    updateMenuState();
-		}else if(currentState == GAME){
-		    updateGameState();
-		}else if(currentState == END){
-		    updateEndState();
+		if (currentState == MENU) {
+			updateMenuState();
+		} else if (currentState == GAME) {
+			updateGameState();
+		} else if (currentState == END) {
+			updateEndState();
 		}
 
-repaint();
+		repaint();
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getKeyCode()==KeyEvent.VK_ENTER) {
-		    if (currentState == END) {
-		        currentState = MENU;
-		    } else {
-		        currentState++;
-		    }
-		} else if (e.getKeyCode()==KeyEvent.VK_UP) {
-	    rocket.up();
-	    } else if (e.getKeyCode()==KeyEvent.VK_DOWN) {
-		    rocket.down();
-		    } else if (e.getKeyCode()==KeyEvent.VK_LEFT) {
-			    rocket.left();
-			    } else if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
-				    rocket.right();	
-				    }
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			if (currentState == END) {
+				currentState = MENU;
+			} else {
+				currentState++;
+			}
+		} else if (e.getKeyCode() == KeyEvent.VK_UP) {
+			rocket.up();
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			rocket.down();
+		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			rocket.left();
+		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			rocket.right();
+		}
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- src/Rocketship.java
+++ src/Rocketship.java
@@ -1,16 +1,27 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 public class Rocketship extends GameObject {
-
+	public static BufferedImage image;
+	public static boolean needImage = true;
+	public static boolean gotImage = false;
 	Rocketship(int x, int y, int width, int height) {
 		super(x, y, width, height);
-		
+		if (needImage) {
+		    loadImage ("rocket.png");
+		}
 	}
 
 	void draw(Graphics g) {
-		g.setColor(Color.BLUE);
-        g.fillRect(x, y, width, height);
+		if (gotImage) {
+			g.drawImage(image, x, y, width, height, null);
+		} else {
+			g.setColor(Color.BLUE);
+			g.fillRect(x, y, width, height);
+		}
 	}
 	public void up(){
 		y-=speed;
@@ -23,5 +34,16 @@ public class Rocketship extends GameObject {
 	}
 	public void right(){
 		x+=speed;
+	}
+	void loadImage(String imageFile) {
+	    if (needImage) {
+	        try {
+	            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+		    gotImage = true;
+	        } catch (Exception e) {
+	            
+	        }
+	        needImage = false;
+	    }
 	}
 }
